ba0f79a028687f399f2d1eb27b97d7b4440e6151
heiron-0497 <charliearaujo97@gmail.com>
2022-09-23 21:58:51 +0200

    Fixes

--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_04.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_04.java
@@ -8,15 +8,15 @@ public class ejercicio_04 {
         System.out.println("A continuación calcularemos el área de un rectángulo.");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Primero indique la base que tendrá el rectángulo: ");
+        System.out.println("Primero indique la base, en centímetros, que tendrá el rectángulo: ");
         b = sc.nextDouble();
 
-        System.out.println("Ahora indique la altura que tendrá el rectángulo: ");
+        System.out.println("Ahora indique la altura,en centímetros, que tendrá el rectángulo: ");
         h = sc.nextDouble();
 
         res = b * h;
 
-        System.out.println("El área del rectángulo con base " + b + " cm2 y altura " + h + " cm2, sería: " + res + " cm2");
+        System.out.println("El área del rectángulo con base " + b + " cm y altura " + h + " cm, sería: " + res + " cm2");
 
     }
 }
--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_05.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_05.java
@@ -8,15 +8,15 @@ public class ejercicio_05 {
         System.out.println("A continuación calcularemos el área de un triángulo.");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Primero indique la base que tendrá el triángulo: ");
+        System.out.println("Primero indique la base, en centímetros, que tendrá el triángulo: ");
         b = sc.nextDouble();
 
-        System.out.println("Ahora indique la altura que tendrá el triángulo: ");
+        System.out.println("Ahora indique la altura, en centímetros, que tendrá el triángulo: ");
         h = sc.nextDouble();
 
         res = (b * h)/2;
 
-        System.out.println("El área del triángulo con base " + b + " cm2 y altura " + h + " cm2, sería: " + res + " cm2");
+        System.out.println("El área del triángulo con base " + b + " cm y altura " + h + " cm, sería: " + res + " cm2");
 
     }
 }
