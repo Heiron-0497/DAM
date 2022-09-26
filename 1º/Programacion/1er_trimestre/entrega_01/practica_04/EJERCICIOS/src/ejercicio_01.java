187a09fd47151205fc5a5865528652b4ccc4cee3
Heiron-0497 <charliearaujo97@gmail.com>
2022-09-26 11:35:00 +0200

    Errors fixed

--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_01.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_01.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ejercicio_01 {
     public static void main(String[] args) {
         
-        final short num = 345;
-        final short per = 20;
-        final short res = num - ((num * per) / 100);
+        final double num = 345;
+        final double per = 20;
+        final double res = num - ((num * per) / 100);
 
         System.out.println(res);
     }
