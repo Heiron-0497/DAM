3b862e22d0cd81f04dffc16c08bd28a6c9e3a255
Heiron-0497 <charliearaujo97@gmail.com>
2022-10-03 11:47:51 +0200

    ADDED exercise 17

--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_17.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_17.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class ejercicio_17 {
     public static void main(String[] args) {
         
-        
+        System.out.println("A continuacion vamos a validar si la fecha de entrega es válida.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Primero introduzca el año de la entrega: ");
     } 
 }
