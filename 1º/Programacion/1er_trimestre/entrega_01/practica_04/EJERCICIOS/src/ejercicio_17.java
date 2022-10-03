63d8ae0ccabe13df3e2fa871f82574d8996685a7
Heiron-0497 <charliearaujo97@gmail.com>
2022-10-03 12:45:42 +0200

    Exercise 17 finished

--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_17.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_17.java
@@ -6,5 +6,40 @@ public class ejercicio_17 {
         System.out.println("A continuacion vamos a validar si la fecha de entrega es válida.");
         Scanner sc = new Scanner(System.in);
         System.out.println("Primero introduzca el año de la entrega: ");
-    } 
+        int year = sc.nextInt();
+
+        if((year >= 1970) && (year <= 2022)) {
+            System.out.println("De acuerdo, ahora introduzca el numero de mes. [1 - 12]");
+            int mon = sc.nextInt();
+            if((mon == 1) || (mon == 3) || (mon == 5) || (mon == 7) || (mon == 8) || (mon == 10) || (mon == 12)) {
+                System.out.println("Introduzca el día del mes. [1 - 31].");
+                int day = sc.nextInt();
+                if((day >= 1) && (day <= 31)) {
+                    System.out.println("Fecha correcta, dentro de rango.");
+                } else {
+                    System.out.println("Fecha fuera de rango.");
+                }
+            } else if((mon == 4) || (mon == 6) || (mon == 9) || (mon == 11)) {
+                System.out.println("Introduzca el día del mes. [1 - 30].");
+                int day = sc.nextInt();
+                if((day >= 1) && (day <= 30)) {
+                    System.out.println("Fecha correcta, dentro de rango.");
+                } else {
+                    System.out.println("Fecha fuera de rango.");
+                }
+            } else if(mon == 2) {
+                System.out.println("Introduzca el día del mes. [1 - 28].");
+                int day = sc.nextInt();
+                if((day >= 1) && (day <= 28)) {
+                    System.out.println("Fecha correcta, dentro de rango.");
+                } else {
+                    System.out.println("Fecha fuera de rango.");
+                }
+            } else {
+                System.out.println("Introduzca un número de mes válido [1- 12]");
+            }
+        } else {
+            System.out.println("El año introducido está fuera de rango.");
+        } sc.close();
+    }
 }
