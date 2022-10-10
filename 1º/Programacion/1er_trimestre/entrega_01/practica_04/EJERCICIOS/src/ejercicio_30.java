abdabc74f328f492d07d1cb4b61f979ad2e6a6f7
Heiron-0497 <charliearaujo97@gmail.com>
2022-10-10 13:36:50 +0200

    updates

--- 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_30.java
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_30.java
@@ -2,9 +2,11 @@ public class ejercicio_30 {
     public static void main(String[] args) {
        int i, j;
        for(i = 1; i < 4; i++) {
-           System.out.println(i);
+           System.out.print(i);
+           System.out.print(" ");
         } for(j = i; j >= 1; j--) {
-            System.out.println(j);
+            System.out.print(j);
+            System.out.print(" ");
         }
     }
 }
---
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_31.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ejercicio_31 {
+    public static void main(String[] args) {
+       
+        final int limite = 10;
+        int num;
+
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("\n\nIntroduzca número: \n");
+            num = sc.nextInt();
+            if (num > 0) {
+                
+            }
+        }
+    }
+}
+
