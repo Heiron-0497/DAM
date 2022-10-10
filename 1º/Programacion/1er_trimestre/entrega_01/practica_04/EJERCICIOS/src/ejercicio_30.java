50b1c45d63a6b4adad5476f80f0fb126c3781a09
Heiron-0497 <charliearaujo97@gmail.com>
2022-10-10 12:04:14 +0200

    Added exercise 30

---
+++ 1º/Programacion/1er_trimestre/entrega_01/practica_04/EJERCICIOS/src/ejercicio_30.java
@@ -0,0 +1,11 @@
+public class ejercicio_30 {
+    public static void main(String[] args) {
+       int i, j;
+       for(i = 1; i < 4; i++) {
+           System.out.println(i);
+        } for(j = i; j >= 1; j--) {
+            System.out.println(j);
+        }
+    }
+}
+
