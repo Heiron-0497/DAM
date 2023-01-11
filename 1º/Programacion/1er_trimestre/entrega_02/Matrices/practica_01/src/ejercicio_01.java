c774f8186c354ba06cef9837a2a9f66fc4c08230
Heiron-0497 <charliearaujo97@gmail.com>
2023-01-11 08:24:17 +0100

    update

--- 1º/Programacion/1er_trimestre/entrega_02/Matrices/practica_01/src/ejercicio_01.java
+++ 1º/Programacion/1er_trimestre/entrega_02/Matrices/practica_01/src/ejercicio_01.java
@@ -3,16 +3,6 @@ import java.util.Scanner;
 public class ejercicio_01 {
     public static void main(String[] args) {
         
-        int a, b, suma;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Escriba el número a: ");
-        a= sc.nextInt();
-        System.out.println("Escriba el número b: ");
-        b= sc.nextInt();
-        sc.close();
-
-        suma = a + b;
-
-        System.out.println("El resultado es: " + suma);
+        
     }
 }
---
+++ 1º/Programacion/2do_trimestre/entrega_01/src/ejercicio_01.java
@@ -0,0 +1,5 @@
+public class ejercicio_01 {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
